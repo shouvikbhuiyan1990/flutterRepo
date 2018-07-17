044ba66a550ae4d13ea47244f7ace5fd4c8edc20
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2018-07-17 21:11:33 +0530

    first widget

--- first_widget/lib/main.dart
+++ first_widget/lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+/***
+ * The code below works perfectly
+ * however we want to introduce stricter type checking and better coding practices
+ * Also we will be using shorthands (arrow Function)
+ ***/
+
+/*
 main() {
   //main method will be called by flutter
   //runApp used to run my module which is MyApp in this case
@@ -7,13 +14,29 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
-  //build is used to paint 
+  //build is used to paint
   //it should always return a widget, can be a custom widget but has to keep returing widget untill it returns a fluter widget
   build(context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Easy Peasy'),
+        ),
+      ),
+    );
+  }
+}
+*/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override //works without saying this, but its a good practice to explicity mention that this method is overriden
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Formatted'),
         ),
       ),
     );
